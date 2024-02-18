efcd5ace3bdf80c1fe1a715f0c78a80be9a9cf82
James <jamesdwire66@gmail.com>
2024-02-18 14:27:58 -0700

    added js and functioality

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ var apiKEY = '8242379471f3c504a5b519fefce4a5f3'
 var search_bar = $('.search-bar')
 var submitBtn = $('.submitBTN')
 var data = search_bar.val()
-// const dayjs = require ('dayjs')
-// var days = {'length': 5,0}
+var list =data.list
+var dt_txt =dt_txt
+// var dateArray = dateArray[0,6,14,21,30,38].split('YYYY-MM-DD')
+// var dateArray = dateTimeArray[0].split('-');
 function getApi() {
     var city = search_bar.val();
-    // var requestURL = "api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid="+apiKEY
+    
     var requestURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKEY
     fetch(requestURL)
         .then(function (response) {
@@ -25,8 +27,9 @@ function getApi() {
             var humidity = data.main.humidity
             var iconCode = data.weather[0].icon;
             var iconURL = "https://openweathermap.org/img/wn/" + iconCode + ".png";
-            // var lat = data.city.coord.lat;
-            // var lon = data.city.coord.lon;
+            var lat = data.coord.lat;
+            var lon = data.coord.lon;
+            var dt_txt =dt_txt
             // $('.search-history').append("<ul><ul>").addClass("ul")
             // $('.search-history').append('<li>' + city + '</li>').addClass("li")
             var exitBtn = $('.deleteBTN')
@@ -44,34 +47,69 @@ function getApi() {
             $('.weather').append(`<p> Wind: ${windMPH} MPH</p>`).addClass('weatherData');
             $('.weather').append(`<p> Humidity: ${humidity}%</p>`).addClass('weatherData');
             $('.weather').css('background-color', 'rgba(255, 255, 255, 0.487)')
+            
 
             search_bar.val("");
             console.log(data.coord)
             console.log(data)
+            fiveday(lat, lon)
 
         })
 }
-// function getGeoCode() {
-//     var city = search_bar.val();
-//     var zip_code = zip_code.val()
-//     var country_code = country_code.val()
-//     var apiKEY = '8242379471f3c504a5b519fefce4a5f3'
-//     var requestGEO = "http://api.openweathermap.org/geo/1.0/zip?zip=" + zip_code + +country_code + "&appid=" + apiKEY
-//     getGeoCode()
-// }
+function fiveday(lat, lon) {
+    var requestURL = "http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid="+apiKEY
+//   dt_txt.split('',38)
+    
+    // var dateArray = [0,6,14,21,30,38].split()
+    fetch( requestURL )
+    .then(function (response) {
+        return response.json();
+        
+    })
+    .then (function(data){
+        console.log(data)
+    })
+    let dateArray = 'YYYY/MM/DD'
+    console.log(list.dt_txt)
+}
+function daysOfWeek(params) {
+    const weekday = [
+        'sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'
+        // 0 sun,1 mon,2 tues,3 wed,4 thurs,5 fri,6 sat,7 sun
+    ]
+    
+    const i = 0
+    const now = new Date();
+    const currentHour = now.getHours();
+    // now.setHours(now.getHours() + 24)
+    if (currentHour >= 5 && currentHour < 12) {
+        timeOfDay = 'Morning'
+
+    } else if (currentHour >= 12 && currentHour < 17) {
+        timeOfDay = 'Afternoon'
+    } else if (currentHour >= 17 && currentHour < 20) {
+        timeOfDay = 'Evening'
+    } else {
+        timeOfDay = 'Night'
+    }
+    now.setHours(now.getHours() + 24)
+    //  const dayindex = (dayindex + 1)%7
+    console.log(currentHour)
+}
+
 submitBtn.on('click', function (event) {
     getApi()
-    // console.log('pong')
-    // getGeoCode()
+    fiveday()
+   
 })
 
 search_bar.on('keyup', function (event) {
     if (event.keyCode === 13) {
         getApi()
-        // getGeoCode()
+        daysOfWeek()
     }
 })
-// FUCK YOU GIT I WANNA BASSH YOUR FUCKING HEAD IN
 
-// i hate gits
+// console.log(data.list[0].dt_txt)
+// i hate git
 
